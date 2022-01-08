5c1d13f1d42442c3d51439f456c9fb587754413b
Shubham Goel <shubhamgoel23@yahoo.com>
2022-01-08 18:33:29 +0530

    format code

--- src/main/java/com/example/shoppinglist/exception/ErrorHandler.java
+++ src/main/java/com/example/shoppinglist/exception/ErrorHandler.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class ErrorHandler implements ErrorController {
 
-	@RequestMapping("/error")
-	public void handleError(HttpServletRequest request) throws Throwable {
-		if (request.getAttribute(RequestDispatcher.ERROR_EXCEPTION) != null) {
-			throw (Throwable) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
-		}
-	}
+    @RequestMapping("/error")
+    public void handleError(HttpServletRequest request) throws Throwable {
+        if (request.getAttribute(RequestDispatcher.ERROR_EXCEPTION) != null) {
+            throw (Throwable) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        }
+    }
 
 }
