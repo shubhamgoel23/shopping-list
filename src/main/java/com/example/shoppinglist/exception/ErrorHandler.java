fd56bfe5c8bc595ef8fd337e4c60a50aa28ba830
Shubham Goel <shubhamgoel23@yahoo.com>
2022-01-08 18:42:14 +0530

    update error handler

--- src/main/java/com/example/shoppinglist/exception/ErrorHandler.java
+++ src/main/java/com/example/shoppinglist/exception/ErrorHandler.java
@@ -5,7 +5,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ErrorHandler implements ErrorController {
 
     @RequestMapping("/error")
