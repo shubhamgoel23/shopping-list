be1f7f304e8b21686dc0a1639abc3df3f3091903
Shubham Goel <shubhamgoel23@yahoo.com>
2022-01-08 16:21:34 +0530

    application setup

--- src/main/java/com/example/shoppinglist/ShoppinglistApplication.java
+++ src/main/java/com/example/shoppinglist/ShoppinglistApplication.java
@@ -1,5 +1,9 @@
 package com.example.shoppinglist;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +12,12 @@ public class ShoppinglistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppinglistApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
